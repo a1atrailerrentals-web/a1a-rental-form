ac76a96262d079f5e8d22454a0ed086fbde57372
a1atrailerrentals-web <a1atrailerrentals@gmail.com>
2026-05-12 19:16:50 -0400

    Add files via upload

---
+++ send-email.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { to, subject, html } = JSON.parse(event.body);
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'a1atrailerrentals@gmail.com',
+      pass: 'onykkjqpmmodxcg'
+    }
+  });
+
+  try {
+    await transporter.sendMail({
+      from: 'A1A Trailer Rentals <a1atrailerrentals@gmail.com>',
+      to,
+      subject,
+      html
+    });
+    return { statusCode: 200, body: JSON.stringify({ success: true }) };
+  } catch (err) {
+    console.log('EMAIL ERROR:', err.message);
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
